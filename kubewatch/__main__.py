7a82664451be5926345c70f5b0ce0d47d00051d5
Frode Hus <frode.hus@outlook.com>
2020-01-24 09:45:32 +0100

    init

---
+++ kubewatch/__main__.py
@@ -0,0 +1,12 @@
+import sys, getopt, urllib3
+from .entities import KubeResources
+from .helpers import Kube
+import adal
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def main():
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
